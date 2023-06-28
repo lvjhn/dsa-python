75884ba78f66ca94caeefc20b77dce6ebc1b01bf
LJ S.A <lvjhn.mx@gmail.com>
2023-06-28 22:40:49 +0800

    Finished fibonacci heap, used modified RosettaCode version.

--- heaps/main_kfh.py
+++ heaps/main_kfh.py
@@ -1,6 +1,6 @@
 from .structs.kfh import KFH
 
-heap = KFH() 
+heap = KFH("max") 
 
 heap.insert("a", 10)
 heap.insert("b", 20)
@@ -13,6 +13,14 @@ heap.insert("h", 80)
 
 heap.display()
 
+heap.update("f", 100)
+
+print("heap.max()", heap.max())
+
 print("No. of items before popping:", heap.count)
 heap.pop()
 print("No. of items after popping:", heap.count)
+
+print("heap.max():", heap.max())
+
+heap.display()
--- heaps/structs/kfh.py
+++ heaps/structs/kfh.py
@@ -10,8 +10,7 @@
         * printable / narrow width
 
         * reference (slightly modified implementation)
-            - https://www.programiz.com/dsa/fibonacci-heap
-            - https://www.programiz.com/dsa/decrease-key-and-delete-node-from-a-fibonacci-heap
+            - https://rosettacode.org/wiki/Fibonacci_heap
 
 		* implements common operations
             - comparator(a, b) 
@@ -34,7 +33,7 @@ class KFH_Item:
         self.key = key 
         self.value = value 
         self.data = data
-        self.order = 0
+        self.degree = 0
         self.mark = False
 
         self.parent = None 
@@ -44,16 +43,28 @@ class KFH_Item:
 
     def add_at_end(self, tree): 
         self.children.append(tree)
-        self.order = self.order + 1 
+        self.degree = self.degree + 1 
 
 class KFH: 
     def __init__(self, type_ = "min"): 
         self.trees = [] 
         self.min_node = None 
+        self.root_list = None
         self.count = 0 
         self.type = type_
         self.key_no = 0 
         self.key_map = {} 
+
+    def iterate(self, head): 
+        node = stop = head 
+        flag = False 
+        while True: 
+            if node == stop and flag is True: 
+                break 
+            elif node == stop: 
+               flag = True 
+            yield node 
+            node = node.right 
     
     def comparator(self, a, b): 
         if self.type == "min": 
@@ -67,101 +78,105 @@ class KFH:
         self.key_map[arr[i].key] = j
         self.key_map[arr[j].key] = i
 
+    def merge_with_root_list(self, node):
+        if self.root_list is None:
+            self.root_list = node
+        else:
+            node.right = self.root_list.right
+            node.left = self.root_list
+            self.root_list.right.left = node
+            self.root_list.right = node
+            
+    def merge_with_child_list(self, parent, node): 
+        if parent.child is None:
+            parent.child = node
+        else:
+            node.right = parent.child.right
+            node.left = parent.child
+            parent.child.right.left = node
+            parent.child.right = node
+    
+    def remove_from_root_list(self, node): 
+        if node == self.root_list:
+            self.root_list = node.right
+        node.left.right = node.right
+        node.right.left = node.left
+        
+    def remove_from_child_list(self, parent, node): 
+        if parent.child == parent.child.right:
+            parent.child = None
+        elif parent.child == node:
+            parent.child = node.right
+            node.right.parent = parent
+        node.left.right = node.right
+        node.right.left = node.left
+
     def insert(self, key, value, data = None):
-        new_node = KFH_Item(key, value, data)
-        self.insert_node(new_node)
+        n = KFH_Item(key, value, data)
+        self.insert_node(n)  
 
     def insert_node(self, node):
-        self.key_map[node.key] = node
+        n = node 
+        n.left = n.right = n 
+        self.key_map[n.key] = n 
 
-        min_node = self.min_node
-
-        if min_node is None:
-            self.min_node = node
-            min_node = self.min_node
-            min_node.left = min_node 
-            min_node.right = min_node 
-        else: 
-            node.right = min_node 
-            node.left = min_node.left 
-            min_node.left.right = node 
-            min_node.left = node
-
-            if self.comparator(node, min_node): 
-                self.min_node = node  
-
+        self.merge_with_root_list(n)
+        if self.min_node is None or self.comparator(n, self.min_node): 
+            self.min_node = n 
         self.count += 1 
 
     def pop(self):
-        smallest = self.min_node 
-
-        del self.key_map[smallest.key]
-
-        if smallest is not None: 
-            child = smallest.child 
-            start = child
-            right = None 
-
-            # transfer children of smallest element 
-            if child is not None:
-                while True:
-                    right = child.right 
-                    self.insert_node(child)
-                    self.count -= 1 
-                    child.parent = None 
-                    child = right 
-                    if child is None and child is start: 
-                        break 
+        z = self.min_node 
+        del self.key_map[z.key]
+        
+        if z is not None: 
+            if z.child is not None: 
+                # attach child nodes to root list 
+                children = [ x for x in self.iterate(z.child) ]
+                for i in range(0, len(children)): 
+                    self.merge_with_root_list(children[i]) 
+                    children[i].parent = None  
+            self.remove_from_root_list(z) 
             
-            # remove smallest element 
-            smallest.left.right = smallest.right 
-            smallest.right.left = smallest.left 
-            smallest.child = None 
-
-            # if heap is to be emptied
-            if smallest is smallest.right: 
-                self.min_node = None 
+            # set new min node in heap 
+            if z == z.right: 
+                self.min_node = self.root_list = None 
             else: 
-                self.min_node = smallest.right 
+                self.min_node = z.right 
                 self.consolidate() 
-            
-            self.count -= 1 
-            return smallest 
-        else:
-            raise Exception("Heap is empty.")
-
-        return None
-
-    def delete(self, key): 
-        pass
+            self.count -= 1
+        return z 
 
     def consolidate(self): 
-        aux = (int(math.log2(self.count)) + 1) * [None]
-        node = self.min_node 
+        A = [None] * self.count 
+        nodes = [w for w in self.iterate(self.root_list)]  
+        for w in range(0, len(nodes)): 
+            x = nodes[w] 
+            d = x.degree 
+            while A[d] != None: 
+                y = A[d]
+                if not self.comparator(x, y): 
+                    x, y = y, x 
+                self.heap_link(y, x) 
+                A[d] = None   
+                d += 1 
+            A[d] = x 
         
-    
-    def link(self, y, x): 
-        y.left.right = y.right 
-        y.right.left = y.left 
+        # find new min node - no need to reconstruct 
+        # new root list below because root list was iteratively 
+        # chaining as we were moving around in the above loop 
+        for i in range(0, len(A)): 
+            if A[i] is not None: 
+                if self.comparator(A[i], self.min_node): 
+                    self.min_node = A[i]
 
-        child = x.child 
-
-        # add y to x's children
-        if child is None: 
-            y.right = y 
-            y.left = y  
-        
-        else: 
-            y.right = child 
-            y.left = child.left              
-            child.left.right = y 
-            child.left = y 
-
-        # attach and y and x together
+    def heap_link(self, y, x): 
+        self.remove_from_root_list(y) 
+        y.left = y.right = y 
+        self.merge_with_child_list(x, y) 
+        x.degree += 1 
         y.parent = x 
-        x.child = y 
-        x.order += 1 
-        y.mark = False 
+        y.mark = False  
         
     def update(self, key, new_value): 
         if key not in self.key_map: 
@@ -177,40 +192,50 @@ class KFH:
             self.update_b(key, new_value)
 
     def update_a(self, key, value): 
-        item = self.key_map[key] 
-        item.value = value 
-        parent = item.parent  
-        
-        if parent is not None and self.comparator(item, parent): 
-            self.cut(item, parent) 
-            self.cascading_cut(parent)
-
-        if self.comparator(item, self.min_node): 
-            self.min_node = item 
+        x = self.get_item(key)
+        x.value = value 
+        y = x.parent  
+        if y is not None and self.comparatro(x, y): 
+            self.cut(x, y)
+            self.cascading_cut(y) 
+        if self.comparator(x, self.min_node): 
+            self.min_node = x
 
     def update_b(self, key, value): 
-        pass 
+        x = self.get_item(key)
+
+        # delete key 
+        self.delete(key)
+
+        # reinsert item with new value to the heap 
+        self.insert(key, value, x.data)
+
+    def delete(self, key): 
+        del_val = None
+        
+        if self.type == "min": 
+            del_val = float("-inf") 
+        elif self.type == "max":  
+            del_val = float("inf") 
+
+        self.update_a(key, del_val)
+        self.pop() 
 
     def cut(self, x, y): 
-        x.right.left = x.left 
-        x.left.right = x.right 
-        y.order = y.order - 1 
-        
-        x.right = None 
-        x.left = None 
-        self.insert_node(x)
-        self.count -=1  
-        x.parent = none 
-        x.mark = False 
+        self.remove_from_child_list(y, x) 
+        y.degree -= 1
+        self.merge_with_root_list(x) 
+        x.parent = None  
+        x.mark = False
 
     def cascading_cut(self, y): 
         z = y.parent 
         if z is not None: 
-            if y.mark == False: 
+            if y.mark is False:
                 y.mark = True 
             else: 
                 self.cut(y, z)
-                self.cascading_cut(z) 
+                self.cascading_cut(z)      
 
     def items(self):
         for key in self.key_map: 
@@ -249,18 +274,25 @@ class KFH:
         self.key_map[key] = item
     
     def display(self):
-        self.display_tree(self.min_node) 
+        self.display_tree(self.root_list) 
     
     def display_tree(self, root, level = 0):
         if root is None: 
             return 
 
-        current = root 
-
-        while True:     
-            print(f"{'    ' * level} {current.value} {current.order}")
+        for current in self.iterate(root): 
+            print(f"{'    ' * level} {current.value} {current.degree}")
             self.display_tree(current.child, level + 1)
             current = current.right
             if current is root: 
                 break  
-            
+
+    def min(self): 
+        if self.type != "min":
+            raise Exception("Not a minimum heap.") 
+        return self.top().value 
+
+    def max(self): 
+        if self.type != "max": 
+            raise Exception("Not a maximum heap.") 
+        return self.top().value 
