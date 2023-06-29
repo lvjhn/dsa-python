e1e9908a7c2648d1491e1ce8e77001c27c206511
LJ S.A <lvjhn.mx@gmail.com>
2023-06-29 13:19:04 +0800

    Readded modified Fibonacci Heap implementation from Rosetta code.

---
+++ heaps/main_kfh.py
@@ -0,0 +1,35 @@
+from .structs.kfh import KFH
+
+heap = KFH("max") 
+
+# heap.insert("a", 10)
+# heap.insert("b", 20)
+# heap.insert("c", 30)
+# heap.insert("d", 40)
+# heap.insert("e", 50)
+# heap.insert("f", 60)
+# heap.insert("g", 70)
+# heap.insert("h", 80)
+
+# heap.display()
+
+# heap.update("f", 100)
+
+# print("heap.max()", heap.max())
+
+# print("No. of items before popping:", heap.count)
+# heap.pop()
+# print("No. of items after popping:", heap.count)
+
+# print("heap.max():", heap.max())
+
+# heap.display()
+
+for i in range(10): 
+    heap.insert(i, i)
+
+for i in range(10):
+    print("----") 
+    heap.pop()
+    heap.display() 
+
